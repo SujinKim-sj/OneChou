14c0d33dd872f95af5e190d5f68e4d68b3ee09dc
cares0 <cares00000@gmail.com>
2022-03-31 10:51:49 +0900

    REFACTOR : ReviewDTO, QnaDTO 공통 부분 상속
    WritingDTO 클래스 생성, 공통 부분은 상속하도록 변경

--- src/main/java/com/onechou/shop/review/ReviewDTO.java
+++ src/main/java/com/onechou/shop/review/ReviewDTO.java
@@ -1,58 +1,16 @@
 package com.onechou.shop.review;
 
-import java.sql.Date;
+import com.onechou.shop.writing.WritingDTO;
 
-public class ReviewDTO {
+public class ReviewDTO extends WritingDTO {
 
-	private Long num;
-	private String contents;
-	private Date regDate;
 	private Integer rating;
-	private String writer;
-	private String memberId;
-	private Long productNum;
-	
-	public Long getNum() {
-		return num;
-	}
-	public void setNum(Long num) {
-		this.num = num;
-	}
-	public String getContents() {
-		return contents;
-	}
-	public void setContents(String contents) {
-		this.contents = contents;
-	}
-	public Date getRegDate() {
-		return regDate;
-	}
-	public void setRegDate(Date regDate) {
-		this.regDate = regDate;
-	}
+
 	public Integer getRating() {
 		return rating;
 	}
 	public void setRating(Integer rating) {
 		this.rating = rating;
 	}
-	public String getWriter() {
-		return writer;
-	}
-	public void setWriter(String writer) {
-		this.writer = writer;
-	}
-	public String getMemberId() {
-		return memberId;
-	}
-	public void setMemberId(String memberId) {
-		this.memberId = memberId;
-	}
-	public Long getProductNum() {
-		return productNum;
-	}
-	public void setProductNum(Long productNum) {
-		this.productNum = productNum;
-	}
-	
+
 }
---
+++ src/main/java/com/onechou/shop/writing/WritingDTO.java
@@ -0,0 +1,51 @@
+package com.onechou.shop.writing;
+
+import java.sql.Date;
+
+public class WritingDTO {
+
+	private Long num;
+	private String contents;
+	private Date regDate;
+	private String writer;
+	private String memberId;
+	private Long productNum;
+	
+	public Long getNum() {
+		return num;
+	}
+	public void setNum(Long num) {
+		this.num = num;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	public Date getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	public Long getProductNum() {
+		return productNum;
+	}
+	public void setProductNum(Long productNum) {
+		this.productNum = productNum;
+	}
+	
+}
