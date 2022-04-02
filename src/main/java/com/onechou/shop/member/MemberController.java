74275e973ae476ae63b661c3bced691854861c00
cares0 <cares00000@gmail.com>
2022-04-02 13:06:52 +0900

    DESIGN : 회원 유형 선택페이지 디자인 수정
    회원 가입 방식 수정, 일반회원인 경우 generalMemberJoin페이지로, 로스터리회원인 경우 roasteryMemberJoin 페이지로 이동하게 설계함

--- src/main/java/com/onechou/shop/member/MemberController.java
+++ src/main/java/com/onechou/shop/member/MemberController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -101,6 +102,11 @@ public class MemberController {
 		model.addAttribute("path", p);
 		String path = "common/result";
 		return path;
+	}
+	
+	@GetMapping("generalMemberJoin")
+	public void generalMemberJoin() throws Exception {
+		
 	}
 	
 	
